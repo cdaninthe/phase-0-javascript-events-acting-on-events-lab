08bbad46f17155056cf3d3f5cbd9b52bb634f700
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2022-01-18 21:12:23 -0500

    commit tree 63d554e96550c12d693569f599499901f88af480

--- index.js
+++ index.js
@@ -11,6 +11,8 @@
   });
   */
 
+
+/*
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
@@ -42,3 +44,42 @@ document.addEventListener("keydown", function (e) {
         }
 });
 
+*/
+
+
+
+
+
+
+
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight(){
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 400-40) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  else if (e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+});
